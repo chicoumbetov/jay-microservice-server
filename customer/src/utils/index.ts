93b87627067b0c3a8e9d0195f2a8f9d612ea6c8e
chicoumbetov <shyngysmaaan@gmail.com>
2024-11-11 11:37:35 +0100

    utils created

---
+++ customer/src/utils/index.ts
@@ -0,0 +1,58 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const { APP_SECRET } = require("../config");
+
+//Utility functions
+export const GenerateSalt = async (): Promise<string> => {
+  return await bcrypt.genSalt();
+};
+
+export const GeneratePassword = async (
+  password: string,
+  salt: string
+): Promise<string> => {
+  return await bcrypt.hash(password, salt);
+};
+
+export const ValidatePassword = async (
+  enteredPassword: string,
+  savedPassword: string,
+  salt: string
+): Promise<boolean> => {
+  return (await GeneratePassword(enteredPassword, salt)) === savedPassword;
+};
+
+export const GenerateSignature = async (
+  payload: object
+): Promise<string | Error> => {
+  try {
+    return await jwt.sign(payload, APP_SECRET, { expiresIn: "30d" });
+  } catch (error) {
+    console.log(error);
+    return error instanceof Error
+      ? error
+      : new Error("Unknown error occurred during signature generation");
+  }
+};
+
+export const ValidateSignature = async (req: any) => {
+  try {
+    const signature = req.get("Authorization");
+    console.log(signature);
+    const payload = await jwt.verify(signature.split(" ")[1], APP_SECRET);
+    req.user = payload;
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+export const FormateData = (data: any) => {
+  if (data) {
+    return { data };
+  } else {
+    throw new Error("Data Not found!");
+  }
+};
