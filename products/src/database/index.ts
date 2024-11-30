de9961b5d4288bc4deab8165e310b6c8f5a74cf5
chicoumbetov <shyngysmaaan@gmail.com>
2024-11-30 10:30:37 +0100

    :sparkles: product ms

---
+++ products/src/database/index.ts
@@ -0,0 +1,8 @@
+// database related modules
+export const databaseConnection = require("./connection");
+
+export const CustomerRepository = require("./repository/customer-repository");
+
+// ! extracted to separate microservice
+// ProductRepository: require("./repository/product-repository"),
+// ShoppingRepository: require("./repository/shopping-repository"),
