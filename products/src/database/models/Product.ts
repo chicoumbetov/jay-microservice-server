10383450b52ed1bf22842652400bb4d25d9727c1
chicoumbetov <shyngysmaaan@gmail.com>
2024-12-02 11:47:36 +0100

    :recycle: refactor ProductSchema type

--- products/src/database/models/Product.ts
+++ products/src/database/models/Product.ts
@@ -3,14 +3,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema({
-  name: String,
-  desc: String,
-  banner: String,
-  type: String,
-  unit: Number,
-  price: Number,
-  available: Boolean,
-  supplier: String,
+  name: { type: String },
+  desc: { type: String },
+  banner: { type: String },
+  type: { type: String },
+  unit: { type: Number },
+  price: { type: Number },
+  available: { type: Boolean },
+  supplier: { type: String },
 });
 
 module.exports = mongoose.model("product", ProductSchema);
