b83d01e4b141d331cbe9012c5e096d6f984697bd
chicoumbetov <shyngysmaaan@gmail.com>
2024-12-02 11:50:41 +0100

    exports

---
+++ shopping/src/api/index.ts
@@ -0,0 +1,4 @@
+module.exports = {
+  appEvent: require("./app-events"),
+  shopping: require("./shopping"),
+};
---
+++ shopping/src/database/index.ts
@@ -0,0 +1,9 @@
+// database related modules
+export const databaseConnection = require("./connection");
+
+export const ShoppingRepository = require("./repository/shopping-repository");
+
+// ! extracted to separate microservice
+// ProductRepository: require("./repository/product-repository"),
+// ShoppingRepository: require("./repository/shopping-repository"),
+// export const CustomerRepository = require("./repository/customer-repository");
